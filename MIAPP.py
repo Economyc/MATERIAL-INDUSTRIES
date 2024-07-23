1b56c25ea1b9a57af7cf4c7b75099b84e3218ab7
OperacionesBukz <operaciones@bukz.co>
2024-07-23 15:14:42 -0500

    act

--- MIAPP.py
+++ MIAPP.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
+import gdown
+import pandas as pd
 import matplotlib.pyplot as plt
 import Inicio
 import POS
