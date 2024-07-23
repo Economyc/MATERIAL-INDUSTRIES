5b9a514ee8e379c43f048e0072c989e589b7bc4e
OperacionesBukz <operaciones@bukz.co>
2024-07-23 13:04:52 -0500

    c

--- MIAPP.py
+++ MIAPP.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-from PIL import Image
-import matplotlib.pyplot as plt
 import Inicio
 import POS
 import Inventario
@@ -24,25 +21,20 @@ class MultiApp:
         })
 
     def run(self):
-        # Añadir imagen
+        # Mostrar el título en negrita en el área principal
+        st.markdown("<h1 style='text-align: center; font-weight: bold;'>MATERIAL INDUSTRIES</h1>", unsafe_allow_html=True)
 
-        with st.sidebar:
-            app = option_menu(
-                menu_title="MATERIAL INDUSTRIES",
-                options=["Inicio", "POS", "Inventario", "Reportes"],
-                icons=['house-fill', 'card-list', 'box-seam-fill', 'bar-chart-fill'],
-                menu_icon='chat-text-fill',
-                default_index=0,  # Establecer el índice por defecto a "Home"
-                styles={
-                    "container": {"padding": "5!important", "background-color": 'black'},
-                    "icon": {"color": "white", "font-size": "23px"}, 
-                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},
-                    "nav-link-selected": {"background-color": "#02ab21"}
-                }
-            )
+        # Crear el menú personalizado
+        st.sidebar.header("MATERIAL INDUSTRIES")
+        selected_option = st.sidebar.selectbox(
+            label="",
+            options=["Inicio", "POS", "Inventario", "Reportes"],
+            format_func=lambda x: f"**{x}**"
+        )
 
+        # Ejecutar la función correspondiente al menú seleccionado
         for app_info in self.apps:
-            if app == app_info["title"]:
+            if selected_option == app_info["title"]:
                 app_info["function"]()
 
 multi_app = MultiApp()
@@ -50,7 +42,6 @@ multi_app.add_app("Inicio", Inicio.app)
 multi_app.add_app("POS", POS.app)
 multi_app.add_app("Inventario", Inventario.app)
 multi_app.add_app("Reportes", Reportes.app)
-
 
 print("Aplicaciones agregadas:", [app_info["title"] for app_info in multi_app.apps])
 
