ad24f3146b4e0d9328a54552352e553210aa801a
OperacionesBukz <operaciones@bukz.co>
2024-07-23 13:08:19 -0500

    act

--- MIAPP.py
+++ MIAPP.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from PIL import Image
-import matplotlib.pyplot as plt
 import Inicio
 import POS
 import Inventario
 import Reportes
 
+# Configuración de la página
 st.set_page_config(
     page_title="MATERIAL INDUSTRIES",
     page_icon="MI.ico"
@@ -25,14 +25,29 @@ class MultiApp:
 
     def run(self):
         # Añadir imagen
+        # Puedes usar st.image para mostrar una imagen si es necesario
+        # Ejemplo: st.image("ruta/a/tu/imagen.png")
 
         with st.sidebar:
+            st.markdown("""
+                <style>
+                /* Cambiar el tamaño y el estilo del botón de despliegue */
+                .css-1d391kg {
+                    font-size: 30px; /* Tamaño del icono */
+                }
+                .css-1d391kg svg {
+                    width: 30px; /* Ajustar el ancho del ícono */
+                    height: 30px; /* Ajustar la altura del ícono */
+                }
+                </style>
+                """, unsafe_allow_html=True)
+
             app = option_menu(
                 menu_title="MATERIAL INDUSTRIES",
                 options=["Inicio", "POS", "Inventario", "Reportes"],
                 icons=['house-fill', 'card-list', 'box-seam-fill', 'bar-chart-fill'],
                 menu_icon='chat-text-fill',
-                default_index=0,  # Establecer el índice por defecto a "Home"
+                default_index=0,  # Establecer el índice por defecto a "Inicio"
                 styles={
                     "container": {"padding": "5!important", "background-color": 'black'},
                     "icon": {"color": "white", "font-size": "23px"}, 
@@ -41,17 +56,20 @@ class MultiApp:
                 }
             )
 
+        # Ejecutar la función correspondiente al menú seleccionado
         for app_info in self.apps:
             if app == app_info["title"]:
                 app_info["function"]()
 
+# Crear y añadir aplicaciones al MultiApp
 multi_app = MultiApp()
 multi_app.add_app("Inicio", Inicio.app)
 multi_app.add_app("POS", POS.app)
 multi_app.add_app("Inventario", Inventario.app)
 multi_app.add_app("Reportes", Reportes.app)
 
-
+# Imprimir aplicaciones agregadas (solo para depuración)
 print("Aplicaciones agregadas:", [app_info["title"] for app_info in multi_app.apps])
 
+# Ejecutar el MultiApp
 multi_app.run()
