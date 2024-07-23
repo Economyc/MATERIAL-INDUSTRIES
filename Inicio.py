a62248d402a1de5aabcb13f6494ed67ee03ef619
OperacionesBukz <operaciones@bukz.co>
2024-07-23 12:28:02 -0500

    Subir repo

---
+++ Inicio.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def app():
+    st.title("Mi Nueva Aplicación")
+    st.subheader("Descripción de la aplicación")
+
+    # Aquí puedes agregar más componentes de Streamlit según sea necesario
+
+# Llama a la función app para ejecutar la aplicación
+if __name__ == "__main__":
+    app()
