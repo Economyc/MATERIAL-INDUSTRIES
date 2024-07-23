0e59f23e42f4b9d97d1ce9ee4e994f6a15f582ae
OperacionesBukz <operaciones@bukz.co>
2024-07-23 13:05:41 -0500

    act

--- MIAPP.py
+++ MIAPP.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import Inicio
 import POS
 import Inventario
@@ -21,20 +22,38 @@ class MultiApp:
         })
 
     def run(self):
-        # Mostrar el título en negrita en el área principal
-        st.markdown("<h1 style='text-align: center; font-weight: bold;'>MATERIAL INDUSTRIES</h1>", unsafe_allow_html=True)
+        # Añadir imagen
+        # Puedes usar st.image para mostrar una imagen si es necesario
+        # Ejemplo: st.image("ruta/a/tu/imagen.png")
 
-        # Crear el menú personalizado
-        st.sidebar.header("MATERIAL INDUSTRIES")
-        selected_option = st.sidebar.selectbox(
-            label="",
-            options=["Inicio", "POS", "Inventario", "Reportes"],
-            format_func=lambda x: f"**{x}**"
-        )
+        with st.sidebar:
+            st.markdown("""
+                <style>
+                .sidebar .sidebar-content {
+                    font-size: 18px; /* Ajustar tamaño del texto */
+                }
+                .sidebar .css-1l7p5m3 {
+                    font-size: 30px; /* Ajustar tamaño del icono */
+                }
+                </style>
+                """, unsafe_allow_html=True)
 
-        # Ejecutar la función correspondiente al menú seleccionado
+            app = option_menu(
+                menu_title="MATERIAL INDUSTRIES",
+                options=["Inicio", "POS", "Inventario", "Reportes"],
+                icons=['house-fill', 'card-list', 'box-seam-fill', 'bar-chart-fill'],
+                menu_icon='chat-text-fill',
+                default_index=0,  # Establecer el índice por defecto a "Inicio"
+                styles={
+                    "container": {"padding": "5!important", "background-color": 'black'},
+                    "icon": {"color": "white", "font-size": "30px"},  # Aumentar tamaño del ícono
+                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "--hover-color": "blue"},
+                    "nav-link-selected": {"background-color": "#02ab21"}
+                }
+            )
+
         for app_info in self.apps:
-            if selected_option == app_info["title"]:
+            if app == app_info["title"]:
                 app_info["function"]()
 
 multi_app = MultiApp()
@@ -46,3 +65,4 @@ multi_app.add_app("Reportes", Reportes.app)
 print("Aplicaciones agregadas:", [app_info["title"] for app_info in multi_app.apps])
 
 multi_app.run()
+
